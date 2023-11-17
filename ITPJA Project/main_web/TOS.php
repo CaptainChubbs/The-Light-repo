9b254c9a6152f71731cac4560dd44901bcf07423
Ben <onthatilebear@gmail.com>
2023-11-17 06:38:56 +0200

    Update TOS.php

--- ITPJA Project/main_web/TOS.php
+++ ITPJA Project/main_web/TOS.php
@@ -16,17 +16,123 @@ include("./head.php");
   <main>
     <section>
       <h2>1. Acceptance of Terms</h2>
-      <p>By accessing or using this website, you agree to comply with and be bound by these Terms of Service.</p>
+      <p>By accessing or using this website, you agree to comply with and be bound by these Terms of Service.
+      Abahlengi is a website that connects users with qualified nurses who can provide various health care services at home or at other locations. By accessing or using our website, you agree to be bound by these terms of service.
+       If you do not agree with these terms, please do not use our website.
+      </p>
     </section>
 
     <section>
       <h2>2. Use of Services</h2>
-      <p>You agree to use the services provided on this website for lawful purposes only and in a manner consistent with all applicable local, national, and international laws and regulations.</p>
+      <p>These terms of service govern your use of our website and any services, products, content, or features that we offer or provide through our website. We may change, modify, add, or remove portions of these terms at any time, with or without notice to you. Your continued use of our website after any changes to these terms means that you accept and agree to the changes. It is your responsibility to check these terms periodically for updates.
+
+2. Registration and Account
+To access some of our services, you may need to register for an account on our website. You agree to provide accurate, current, and complete information during the registration process and to update your information as necessary. You are responsible for maintaining the security and confidentiality of your account and password. You are also responsible for any activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account or any breach of security. We reserve the right to suspend or terminate your account at any time for any reason, with or without notice.
+
+3. Services and Fees
+We provide a platform that enables you to find and book nurses who can provide various health care services at your preferred location and time. We do not employ, endorse, or guarantee the quality, reliability, or suitability of any nurse or service that you book through our website. You are solely responsible for selecting, communicating with, and paying the nurse for the services that you receive. You agree to comply with the terms and conditions of the nurse that you book, including any cancellation or rescheduling policies.
+
+We charge a service fee for each booking that you make through our website. The service fee is calculated as a percentage of the total amount that you pay to the nurse for the service. The service fee is non-refundable, unless the booking is cancelled by the nurse or by us. We reserve the right to change the service fee at any time, with or without notice to you.
+
+You agree to pay the service fee and the amount due to the nurse using the payment methods that we accept on our website. You authorize us to charge your payment method for the service fee and to transfer the amount due to the nurse on your behalf. You are responsible for any taxes, fees, or charges that may apply to your payments. You agree to pay any amounts that are overdue, disputed, or charged back. We reserve the right to collect any amounts that you owe us by any lawful means, including by using third-party collection agencies.
+
+4. User Conduct and Content
+You agree to use our website and services in a lawful, respectful, and responsible manner. You agree not to:
+
+- Violate any applicable laws, regulations, or ethical standards
+- Impersonate any person or entity, or misrepresent your identity or affiliation
+- Harass, abuse, threaten, defame, or otherwise harm any person or entity
+- Post, upload, transmit, or share any content that is unlawful, harmful, offensive, obscene, pornographic, violent, hateful, discriminatory, or otherwise objectionable
+- Infringe or violate any intellectual property rights, privacy rights, or other rights of any person or entity
+- Interfere with or disrupt the operation or security of our website or services, or any networks or servers connected to them
+- Transmit or introduce any viruses, malware, or other harmful or malicious code or devices
+- Attempt to gain unauthorized access to any accounts, systems, or data
+- Use any automated means, such as bots, scripts, or scrapers, to access, collect, or manipulate any data or content from our website or services
+- Use our website or services for any commercial or fraudulent purposes, or in any manner that competes with or harms us or our business
+
+We do not monitor, review, or endorse any content that you or other users post, upload, transmit, or share on or through our website or services. However, we reserve the right to remove, edit, or block any content that we deem to be in violation of these terms or any applicable laws, regulations, or standards. We also reserve the right to report any content or activity that we suspect to be illegal, unethical, or harmful to the relevant authorities.
+
+You retain the ownership and responsibility of any content that you post, upload, transmit, or share on or through our website or services. By doing so, you grant us a non-exclusive, royalty-free, worldwide, perpetual, irrevocable, and sublicensable license to use, reproduce, modify, adapt, publish, distribute, display, and perform your content for any purpose related to our website or services. You also waive any moral rights or claims that you may have in your content.
+
+5. Intellectual Property Rights
+We own and retain all rights, title, and interest in and to our website, services, trademarks, logos, and any content, features, or materials that we provide or make available on or through our website or services. Nothing in these terms grants you any right, title, or interest in or to any of our intellectual property rights, except for the limited license to use our website and services as expressly provided in these terms. You agree not to copy, modify, distribute, sell, or otherwise exploit any of our intellectual property rights without our prior written consent.
+
+6. Disclaimer of Warranties
+Our website and services are provided "as is" and "as available" without any warranties of any kind, either express or implied. To the fullest extent permitted by law, we disclaim all warranties, including, but not limited to, the warranties of merchantability, fitness for a particular purpose, non-infringement, and title. We do not warrant that our website or services will be uninterrupted, error-free, secure, accurate, reliable, or free of viruses or other harmful components. You assume the entire risk of using our website or services and any results or outcomes that may arise from such use.
+
+7. Limitation of Liability
+To the fullest extent permitted by law, we and our affiliates, directors, officers, employees, agents, contractors, and licensors will not be liable for any direct, indirect, incidental, consequential, special, exemplary, or punitive damages, or any other damages of any kind, arising out of or in connection with your use of or inability to use our website or services, or any content, products, or services obtained or accessed through our website or services, whether based on contract, tort, negligence, strict liability, or any other legal theory, even if we have been advised of the possibility of such damages. In no event will our total liability to you for all damages, losses, and causes of action exceed the amount paid by you, if any, for accessing or using our website or services during the twelve months preceding the date of your claim.
+
+Some jurisdictions do not allow the exclusion or limitation of certain damages, so some or all of the above exclusions or limitations may not apply to you.
+
+8. Indemnification
+You agree to indemnify, defend, and hold harmless us and our affiliates, directors, officers, employees, agents, contractors, and licensors from and against any and all claims, liabilities, damages, losses, costs, expenses, or fees (including reasonable attorneys' fees) that arise from or relate to your use of or access to our website or services, or your violation of these terms or any applicable laws, regulations, or standards. We reserve the right to assume the exclusive defense and control of any matter that is subject to indemnification by you, and you agree to cooperate with our defense of such claims. You agree not to settle any such claim without our prior written consent.
+
+9. Dispute Resolution
+Any dispute, claim, or controversy arising out of or relating to these terms or your use of or access to our website or services will be resolved by binding arbitration, except that you or we may seek injunctive or other equitable relief in any court of competent jurisdiction for any actual or threatened breach of intellectual property rights or other urgent matters. You agree that any arbitration will take place on an individual basis and not on a class, consolidated, or representative basis. You also agree to waive any right to a jury trial or to participate in a class action. These terms will be governed by and construed in accordance with the laws of South Africa, without regard to its conflict of laws principles. Any legal action or proceeding related to these terms or your use of or access to our website or services will be brought exclusively in the courts of South Africa, and you consent to the personal jurisdiction and venue of such courts.
+
+10. General Provisions
+These terms constitute the entire agreement between you and us regarding your use of and access to our website and services, and supersede any prior or contemporaneous agreements, communications, or understandings, whether written or oral, on the same subject matter. If any provision of these terms is held to be invalid, illegal, or unenforceable by a court of competent jurisdiction, such provision will be modified to the extent necessary to make it enforceable, or if not possible, will be severed from these terms, and the remaining provisions will remain in full force and effect. Our failure to exercise or enforce any right or provision of these terms will not constitute a waiver of such right or provision. We may assign or transfer our rights and obligations under these terms to any person or entity without your consent. You may not assign or transfer your rights and obligations under</p>
     </section>
 
     <section>
       <h2>3. Privacy Policy</h2>
-      <p>Your use of our services is also governed by our Privacy Policy. Please review our Privacy Policy to understand how we collect, use, and disclose information.</p>
+      <p>
+What information do we collect?
+
+We collect information that you voluntarily provide to us when you fill out a contact form, request a quote, subscribe to our newsletter, or register for an account on our website. This information may include your name, email address, phone number, location, and any other details that you choose to share with us.
+
+We also collect information that is automatically generated when you browse our website, such as your IP address, browser type, device type, operating system, pages viewed, time spent, and other statistical data. We use cookies and other tracking technologies to collect this information and to enhance your user experience on our website.
+
+How do we use your information?
+
+We use your information for the following purposes:
+
+- To provide you with the information and services that you request from us
+- To communicate with you about our website, services, offers, and promotions
+- To improve our website, services, and marketing strategies
+- To analyze our website traffic and user behavior
+- To comply with our legal obligations and protect our rights and interests
+
+How do we share your information?
+
+We do not sell, rent, or trade your information with any third parties for their own marketing purposes. We may share your information with the following parties for the purposes described above:
+
+- The nurses that you book through our website, who need your information to provide you with the services that you request
+- Our service providers, contractors, and partners who help us operate our website and provide our services
+- Our affiliates and subsidiaries who offer products or services that may be of interest to you
+- Our professional advisors, auditors, and lawyers who assist us with our legal and financial matters
+- Law enforcement, regulators, and courts when required by law or when necessary to protect our rights and interests
+
+How do we protect your information?
+
+We take reasonable measures to protect your information from unauthorized access, use, disclosure, alteration, or destruction. We use secure servers, encryption, firewalls, and other technical and organizational safeguards to protect your information. However, no method of transmission or storage is 100% secure and we cannot guarantee the absolute security of your information.
+
+What are your choices and rights?
+
+You have the following choices and rights regarding your information:
+
+- You can opt-out of receiving marketing emails from us by clicking on the unsubscribe link in the email or by contacting us at info@abahlengi.com
+- You can update, correct, or delete your account information by logging into your account or by contacting us at info@abahlengi.com
+- You can request access to, or a copy of, the information that we hold about you by contacting us at info@abahlengi.com
+- You can object to, or restrict, our processing of your information for certain purposes by contacting us at info@abahlengi.com
+- You can withdraw your consent to our processing of your information at any time by contacting us at info@abahlengi.com
+- You can lodge a complaint with the relevant data protection authority if you believe that we have violated your privacy rights
+
+How do we update our privacy policy?
+
+We may update our privacy policy from time to time to reflect changes in our practices, technologies, or legal requirements. We will notify you of any material changes by posting a notice on our website or by sending you an email. Your continued use of our website or services after the effective date of the updated privacy policy constitutes your acceptance of the changes.
+
+How can you contact us?
+
+If you have any questions or comments about our privacy policy or our data practices, please contact us at:
+
+Abahlengi
+123 Main Street
+Johannesburg, Gauteng, South Africa
+info@abahlengi.com
++27 11 123 4567
+</p>
     </section>
 
     <!-- Add more sections as needed -->
