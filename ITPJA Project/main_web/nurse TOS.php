fe76dbdc9dcb4f5a3df285b01f5f0107ce8a37f9
Ben <onthatilebear@gmail.com>
2023-11-17 06:57:53 +0200

    Create nurse TOS.php

---
+++ ITPJA Project/main_web/nurse TOS.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php 
+$title = "TOS";
+
+include("./head.php");
+?> 
+</head>
+<body>
+
+  <header>
+    <h1>Terms of Service</h1>
+  </header>
+
+  <main>
+    <section>
+      <h2>1. Acceptance of Terms</h2>
+      <p>By accessing or using this website, you agree to comply with and be bound by these Terms of Service.
+      Abahlengi is a website that connects users with qualified nurses who can provide various health care services at home or at other locations. By accessing or using our website, you agree to be bound by these terms of service.
+       If you do not agree with these terms, please do not use our website.
+      </p>
+    </section>
+
+    <section>
+      <h2>2. Use of Services</h2>
+      <p>Eligibility and Registration
+To use our site, you must be a registered nurse in South Africa. You must also have a valid license and registration number issued by the South African Nursing Council (SANC) or the Health Professions Council (HPC). You must provide accurate and complete information about yourself and your qualifications when you register on our site.
+
+Services and Fees
+We offer various services for nurses, such as home care, hospital care, travel nursing, and more. We charge fees for each service based on the type, duration, and location of the service. The fees are displayed on our site before you book a service. You agree to pay the fees in advance or upon completion of the service.
+
+We do not guarantee the availability or quality of any service offered on our site. We are not responsible for any damages, injuries, or losses that may occur as a result of using our services. We are not liable for any errors or omissions in the information provided on our site.
+
+Privacy and Security
+We respect your privacy and security when you use our site. We do not collect, store, or share any personal information about you without your consent. We use encryption and other security measures to protect your data from unauthorized access or misuse.
+
+You are responsible for keeping your password and account information confidential. You must not share your password or account information with anyone else. You must notify us immediately if you suspect any unauthorized activity on your account.
+
+Intellectual Property Rights
+We own all the intellectual property rights to our website, logo, trademarks, and other content on our site. You do not have any rights to use or copy any of these content without our permission.
+
+You grant us a non-exclusive, royalty-free, worldwide license to use your name, image, voice, and content that you provide when you register on our site for promotional purposes.
+
+Dispute Resolution
+If you have any questions or complaints about these terms of service or our services, please contact us at info@abahlengi.com.
+
+These terms of service are governed by the laws of South Africa. Any disputes arising from these terms of service or our services shall be subject to the exclusive jurisdiction of the courts of South Africa.
+
+By using our site, you accept these terms of service and agree to abide by them.</p>
+    </section>
+
+    <section>
+      <h2>3. Privacy Policy</h2>
+      <p>
+What information do we collect?
+
+We collect information that you voluntarily provide to us when you fill out a contact form, request a quote, subscribe to our newsletter, or register for an account on our website. This information may include your name, email address, phone number, location, and any other details that you choose to share with us.
+
+We also collect information that is automatically generated when you browse our website, such as your IP address, browser type, device type, operating system, pages viewed, time spent, and other statistical data. We use cookies and other tracking technologies to collect this information and to enhance your user experience on our website.
+
+How do we use your information?
+
+We use your information for the following purposes:
+
+- To provide you with the information and services that you request from us
+- To communicate with you about our website, services, offers, and promotions
+- To improve our website, services, and marketing strategies
+- To analyze our website traffic and user behavior
+- To comply with our legal obligations and protect our rights and interests
+
+How do we share your information?
+
+We do not sell, rent, or trade your information with any third parties for their own marketing purposes. We may share your information with the following parties for the purposes described above:
+
+- The nurses that you book through our website, who need your information to provide you with the services that you request
+- Our service providers, contractors, and partners who help us operate our website and provide our services
+- Our affiliates and subsidiaries who offer products or services that may be of interest to you
+- Our professional advisors, auditors, and lawyers who assist us with our legal and financial matters
+- Law enforcement, regulators, and courts when required by law or when necessary to protect our rights and interests
+
+How do we protect your information?
+
+We take reasonable measures to protect your information from unauthorized access, use, disclosure, alteration, or destruction. We use secure servers, encryption, firewalls, and other technical and organizational safeguards to protect your information. However, no method of transmission or storage is 100% secure and we cannot guarantee the absolute security of your information.
+
+What are your choices and rights?
+
+You have the following choices and rights regarding your information:
+
+- You can opt-out of receiving marketing emails from us by clicking on the unsubscribe link in the email or by contacting us at info@abahlengi.com
+- You can update, correct, or delete your account information by logging into your account or by contacting us at info@abahlengi.com
+- You can request access to, or a copy of, the information that we hold about you by contacting us at info@abahlengi.com
+- You can object to, or restrict, our processing of your information for certain purposes by contacting us at info@abahlengi.com
+- You can withdraw your consent to our processing of your information at any time by contacting us at info@abahlengi.com
+- You can lodge a complaint with the relevant data protection authority if you believe that we have violated your privacy rights
+
+How do we update our privacy policy?
+
+We may update our privacy policy from time to time to reflect changes in our practices, technologies, or legal requirements. We will notify you of any material changes by posting a notice on our website or by sending you an email. Your continued use of our website or services after the effective date of the updated privacy policy constitutes your acceptance of the changes.
+
+How can you contact us?
+
+If you have any questions or comments about our privacy policy or our data practices, please contact us at:
+
+Abahlengi
+123 Main Street
+Johannesburg, Gauteng, South Africa
+info@abahlengi.com
++27 11 123 4567
+</p>
+    </section>
+
+    <!-- Add more sections as needed -->
+
+  </main>
+
+  
+  <?php include_once __DIR__.'/footer.php'; ?>
+
+</body>
+</html>
